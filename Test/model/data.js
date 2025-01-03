eed28a7047367cdbb69afb1ee2fd07c80d55c01b
Xrtion <kicaubae@gmail.com>
2025-01-03 23:38:15 +0700

    updated

--- Test/model/data.js
+++
@@ -1,9 +0,0 @@
-const songs = [
-    {
-        id: 1,
-        title: 'Beauty And A Beat',
-        artist: 'Justin Bieber',
-        artwork: require('../assets/img/BeautyAndABeat.png'),
-        url: ''
-    },
-]
